a0d62f9a5bf31ce9053045827fa26e3553284ba2
Mehrdad-R <mrakhshieh@gmail.com>
2026-03-02 19:38:20 -0800

    Remove venv from repository and add .gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kylea\Downloads\appointment-booking-ai-testing\venv
+set VIRTUAL_ENV=C:\Users\mrakh\OneDrive\Documents\GitHub\appointment-booking-ai-testing\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
